7d1be0fcbc65d7888e39e8c84e2590f5510e223b
cardissuer <cardissuer@gmail.com>
2020-02-28 14:12:48 -0600

    28 Feb version

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -8,12 +8,12 @@ import org.junit.runner.RunWith;
 @RunWith( Cucumber .class)
     @CucumberOptions(  // features and glue specifications have to be here
 
-            features = "src/test/resources/features",  //we are connecting features to features folder in the framework
-            glue = "step_definitions",  //specifiying steps definitions here specifications make connection between code and steps
-            plugin = "html:target/cucumber-reports",  // it generates html reports in target folder
-            dryRun = true   //  you use dryRun = true do not execute the steps to see required implementations you can copy and implement in stepDefs
+           features = "src/test/resources/features",  //we are connecting features to features folder in the framework
+           glue = "step_definitions",  //specifiying steps definitions here specifications make connection between code and steps
+           plugin = "html:target/cucumber-reports",  // it generates html reports in target folder
+           dryRun = false,   //  you use dryRun = true do not execute the steps to see required implementations you can copy and implement in stepDefs
 //by default dryRun is false it means run the scenario and execute the steps
-           // tags = "@smoke" // when you want to run specific scenario
+           tags = "@lostReasonsSearch or @lostReasonsNameCheckbox or @lostReasonsCreateImportButton" // when you want to run specific scenario
 
     )
 
@@ -22,6 +22,3 @@ import org.junit.runner.RunWith;
 
 
     }
-
-
-
